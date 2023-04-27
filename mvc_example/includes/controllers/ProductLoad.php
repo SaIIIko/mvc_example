4acd3145182ac089eab06eb02833816621f8da19
saiiiko <al.voevodsliy@gmail.com>
2023-04-27 12:45:18 +0300

    INT-6: Добавить класс для передачи массива значений полей в hook_menu page callback

---
+++ mvc_example/includes/controllers/ProductLoad.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Class represents loader for hook menu callback
+ */
+class ProductLoad {
+
+  /**
+   * Create static method for specific hook_menu "page callback" requirements
+   * @throws \Exception
+   * @return array
+   */
+  public static function load($id) {
+    $product = new ProductBuild($id);
+    return $product->buildFieldsArray();
+  }
+}
