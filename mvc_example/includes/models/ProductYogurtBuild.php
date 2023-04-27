746c5ffa0edd21cfd405c075c7cc98a86b699295
saiiiko <al.voevodsliy@gmail.com>
2023-04-27 16:03:05 +0300

    INT-6: Перенести файлы в соответствующие места по принципу MVC и переименовать адрес в подключаемых файлах

--- mvc_example/includes/models/ProductYogurtBuild.php
+++ mvc_example/includes/models/ProductYogurtBuild.php
@@ -41,4 +41,23 @@ class ProductYogurtBuild extends Product {
     }
   }
 
+  /**
+   * Get all product fields and push array to template
+   *
+   * @return mixed
+   * @throws \Exception
+   */
+  public function buildFieldsArray() {
+    $fields_arr = $this->getProductCustomFields();
+    $fields_arr['id'] = $this->getId();
+    $fields_arr['title'] = $this->getName();
+    $fields_arr['brand'] = $this->getBrand();
+    $fields_arr['price'] = $this->getPrice();
+    $fields_arr['manufacture_date'] = $this->getManufactureDate();
+    $fields_arr['expiration_date'] = $this->getExpirationDate();
+
+    $output = theme('product_template', array('data' => $fields_arr));
+    return $output;
+  }
+
 }
