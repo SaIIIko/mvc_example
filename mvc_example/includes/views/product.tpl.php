003aaf2550e20bbe15c21364ba57b928e7321a24
saiiiko <al.voevodsliy@gmail.com>
2023-04-27 12:53:36 +0300

    INT-6: Добавить структуру и вывод полей в шаблоне

---
+++ mvc_example/includes/views/product.tpl.php
@@ -0,0 +1,46 @@
+<div class="product__full product-yogurt__full">
+  <div class="product__full-wrap">
+    <div class="product__left">
+      <img src="/sites/all/modules/custom/mvc_example/img/no_image.jpg" alt="">
+    </div>
+    <div class="product__right">
+      <h1 class="page-title"><?php print $data['title'] ?></h1>
+      <?php if ($data['price']): ?>
+        <div class="attr-item attr-item__price">
+          <div class="value"><?php print $data['price'] ?> руб.</div>
+        </div>
+      <?php endif; ?>
+      <?php if ($data['brand']['name']): ?>
+        <div class="attr-item">
+          <div class="title">Бренд:</div>
+          <div class="value"><?php print $data['brand']['name'] ?></div>
+        </div>
+      <?php endif; ?>
+      <?php if ($data['manufacture_date']): ?>
+        <div class="attr-item">
+          <div class="title">Дата изготовления:</div>
+          <div class="value"><?php print $data['manufacture_date'] ?></div>
+        </div>
+      <?php endif; ?>
+      <?php if ($data['expiration_date']): ?>
+        <div class="attr-item">
+          <div class="title">Срок годности:</div>
+          <div class="value">
+            <?php if ($data['expiration_date'] != 'end'): ?>
+              <?php print 'До истечения срока годности остало дней -  '.$data['expiration_date'] ?>
+            <?php else: ?>
+              <?php print 'Срок годности истек!' ?>
+            <?php endif; ?>
+
+          </div>
+        </div>
+      <?php endif; ?>
+    </div>
+  </div>
+  <?php if ($data['body_value']): ?>
+    <div class="description-item">
+      <div class="title">Описание:</div>
+      <div class="value"><?php print $data['body_value'] ?></div>
+    </div>
+  <?php endif; ?>
+</div>
