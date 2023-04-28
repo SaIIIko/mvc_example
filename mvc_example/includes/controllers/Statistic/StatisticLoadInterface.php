1f3a45532c5760aa211755a078748507c9746245
saiiiko <al.voevodsliy@gmail.com>
2023-04-28 13:22:28 +0300

    INT-6: Добавить интерфейс контроллера загрузки данных в шаблон

---
+++ mvc_example/includes/controllers/Statistic/StatisticLoadInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @file
+ * Interface for adding data to the template
+ */
+
+interface StatisticLoadInterface {
+
+  /**
+   * Load data to template
+   *
+   * @return mixed
+   */
+  public static function load();
+}
