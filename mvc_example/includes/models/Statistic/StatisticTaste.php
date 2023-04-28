4e4841def26fca5190e7ba562fbd6999c1ff8b8f
saiiiko <al.voevodsliy@gmail.com>
2023-04-28 12:48:48 +0300

    INT-6: Создать вывод всех продуктов имеющих вкус

---
+++ mvc_example/includes/models/Statistic/StatisticTaste.php
@@ -0,0 +1,16 @@
+<?php
+
+class StatisticTaste implements StatisticInterface {
+
+  public function request() {
+    $result =  db_query("
+      SELECT t.name,
+             n.title
+      FROM {taxonomy_term_data} t
+      RIGHT JOIN {field_data_product_taste} ft ON t.tid = ft.product_taste_tid
+      INNER JOIN {node} n ON n.nid = ft.entity_id
+      ")->fetchAll(PDO::FETCH_ASSOC);
+
+    return $result;
+  }
+}
