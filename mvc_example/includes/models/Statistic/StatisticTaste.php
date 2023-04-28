86e174fdc788ca1d544e57229593bda2b048e1a4
saiiiko <al.voevodsliy@gmail.com>
2023-04-28 17:09:53 +0300

    INT-6: Изменить модель выборки продуктов с вариациями вкусов. Демонстрация работы CROSS JOIN

--- mvc_example/includes/models/Statistic/StatisticTaste.php
+++ mvc_example/includes/models/Statistic/StatisticTaste.php
@@ -8,18 +8,18 @@
 class StatisticTaste implements StatisticInterface {
 
   /**
-   * Get product and taste names from db
+   * Get product and taste variations
    *
    * @return array|null
    */
   public function request() {
     $result =  db_query("
-      SELECT t.name,
-             n.title
+      SELECT t.name, n.title
       FROM {taxonomy_term_data} t
       INNER JOIN {field_data_product_taste} ft ON t.tid = ft.product_taste_tid
-      INNER JOIN {node} n ON n.nid = ft.entity_id
-      ")->fetchAll(PDO::FETCH_ASSOC);
+      CROSS JOIN {node} n
+      WHERE n.type = 'products'"
+    )->fetchAll(PDO::FETCH_ASSOC);
 
     return $result;
   }
